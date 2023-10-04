5fed316c174cc5f11ba9c9c8732671a0ff855374
Felix Reuthlinger <10428747+FelixReuthlinger@users.noreply.github.com>
2023-10-04 14:01:47 +0200

    1.2.3

--- PrefabDependencyTree/CHANGELOG.md
+++ PrefabDependencyTree/CHANGELOG.md
@@ -1,4 +1,4 @@
-* 1.2.1 & 2 -> updated dependencies
+* 1.2.1 & 2 & 3 -> updated dependencies, now with Jotunn fix this mod should also work as expected
 * 1.2.0 ->
   * some fixes for filtering (added unfiltered option for just printing all)
   * added GraphViz conversion call (if installed)
--- PrefabDependencyTree/PrefabDependencyTreePlugin.cs
+++ PrefabDependencyTree/PrefabDependencyTreePlugin.cs
@@ -13,7 +13,7 @@ namespace PrefabDependencyTree
         public const string PluginAuthor = "FixItFelix";
         public const string PluginGUID = PluginAuthor + "." + PluginName;
         public const string PluginName = "PrefabDependencyTree";
-        public const string PluginVersion = "1.2.2";
+        public const string PluginVersion = "1.2.3";
 
         private void Awake()
         {
